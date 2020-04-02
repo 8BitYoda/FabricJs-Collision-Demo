5144ee252f4a584aa463024fe38af429a8547e08
Martin Hutchens <mhutchens@captechconsulting.com>
2020-04-02 11:52:33 -0400

    Updates

---
+++ precache-manifest.e0225672b2fdf620db3efd778db9d494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bb58815241efbc1f8e936266cf6472",
+    "url": "/FabricJs-Collision-Demo/index.html"
+  },
+  {
+    "revision": "bc1e50f380dc65889ecb",
+    "url": "/FabricJs-Collision-Demo/static/css/main.3d47ee05.chunk.css"
+  },
+  {
+    "revision": "6d182b663cec63fad1a6",
+    "url": "/FabricJs-Collision-Demo/static/js/2.763d74f9.chunk.js"
+  },
+  {
+    "revision": "0e5e42abd41a0edd7268b14d1f53960b",
+    "url": "/FabricJs-Collision-Demo/static/js/2.763d74f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1e50f380dc65889ecb",
+    "url": "/FabricJs-Collision-Demo/static/js/main.2ea2b2e9.chunk.js"
+  },
+  {
+    "revision": "da7fe8000ec2e1093f1a",
+    "url": "/FabricJs-Collision-Demo/static/js/runtime-main.24125279.js"
+  }
+]);
